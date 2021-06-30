456ed70baf485f679de52a682dbddb951cd418d6
ZemskovNikita <n.zemskov2013@yandex.ru>
2021-06-30 20:06:14 +0300

    Сделал задание 3-1 с отчётом

---
+++ Work_3/Task 3-1/Task 3-1.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+
+/**
+ * \brief функция для расчета заначений табуляции.
+ * \x начало интервала.
+ * \return значение табуляции.
+*/
+double Tabulation(const double x);
+/**
+ * \brief Возможность выполнения функции.
+ * \return в случае успеха, возвращает 1.
+*/
+bool Luck(const double x);
+
+/**
+ * \brief Вход в программу.
+ * \return в случае успеха, возвращает 0.
+*/
+int main() {
+	double x = 1.0;
+	const auto z = 3;
+	const auto step = 0.2;
+
+	for (x; x <= z; x += step) {
+		if (Luck(x))
+			cout << "x: " << x << " y: " << Tabulation(x) << endl;
+	}
+
+	system("pause");
+	return 0;
+}
+
+double Tabulation(double x) {
+	return sin(log(x)) - cos(log(x)) + 2 * log(x);
+}
+bool Luck(double x) {
+	return 1 / (sin(log(x)) - cos(log(x)) + 2 * log(x)) != 0;
+}
